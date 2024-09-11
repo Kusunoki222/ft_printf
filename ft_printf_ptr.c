9da38ace69fd6ea5e4f74f9a61b9dad44f09c02e
Kotaro Kusunoki <kkusunok@c3r6s4.42tokyo.jp>
2024-09-11 17:43:56 +0900

    syuusei

--- ft_printf_ptr.c
+++ ft_printf_ptr.c
@@ -6,7 +6,7 @@
 /*   By: kkusunok <kkusunok@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 19:04:40 by kkusunok          #+#    #+#             */
-/*   Updated: 2024/09/11 16:44:51 by kkusunok         ###   ########.fr       */
+/*   Updated: 2024/09/11 17:43:31 by kkusunok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 int	ft_printf_ptr(unsigned long long ptr)
 {
-	printf("What's mine? %p\n", ptr);
-	return(0);
+	int	count;
+
+	count = 0;
+	if (!ptr)
+		return (write(1, "(nil)", 5));
+	count += write(1, "0x", 2);
+	
+	return (ft_strlen(ptr));
 }
